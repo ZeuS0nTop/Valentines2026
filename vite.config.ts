f61003fcb92204421df33322ff7729d0701c3e69
Krishna <krishna.tiwari@vsit.edu.in>
2026-02-13 17:09:12 +0530

    Final

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  base: "/Valentines2026/", // 👈 IMPORTANT
+
+  plugins: [react()],
+
+  root: path.resolve(__dirname, "client"),
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "client", "src"),
+    },
+  },
+
+  build: {
+    outDir: path.resolve(__dirname, "dist"),
+    emptyOutDir: true,
+  },
+
+  server: {
+    port: 5173,
+    open: true,
+  },
+});
